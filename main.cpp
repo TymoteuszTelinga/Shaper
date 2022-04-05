50a6145304e6cd1b541f87a318fca34bbd78e334
TymTel <tymjanek2001@gmail.com>
2022-04-05 15:17:15 +0200

    minor code cleanup

--- main.cpp
+++ main.cpp
@@ -13,157 +13,6 @@ using std::string;
 const string single = ".,:;()[]{}&|";
 const string doubleOperators = "-+*/%=!<>";
 
-bool checkForKeyWord(const string&);
-
-//dodaje wystapienie slowa kluczowego do licznika
-void addToken(std::map<string,int>& counter, const string& keyword)
-{
-    auto search = counter.find(keyword);
-
-    if(search != counter.end())
-    {
-        ++search->second;
-    }
-    else
-    {
-        counter.emplace(std::pair{keyword,1});
-    }
-}
-
-void printCounter(std::map<string, int>& counter)
-{
-    for (const auto& keyword : counter)
-    {
-        std::cout<<keyword.first <<": " <<keyword.second<<"\n";
-    }
-}
-
-//zapisuje słowo do listy wyrażeń
-void saveToken(std::vector<string>& t,string& word)
-{
-    if (word.size() == 0)
-    {
-        return;
-    }
-
-    t.push_back(word);
-
-    word.clear();
-}
-//zapisuje znak do listy wyrażeń
-void saveToken(std::vector<string>& t,char c)
-{
-    t.push_back(string(1,c));
-}
-//odczytuje następny znak z słowa
-char next(const string& line, const int pos)
-{
-    if (pos >= line.size())
-    {
-        return -1;
-    }
-
-    return line[pos+1];
-}
-//zwraca liste zawierającą poszczegolne "wyrażenia"
-std::vector<string> createTokensFromFile(const string& src)
-{
-    std::vector<string> tokens;
-    std::map<string,int> counter;
-
-    std::ifstream file(src);
-    if (!file.is_open())
-    {
-        cerr<<"error: "<<src<<": No such file or directory\n";
-        return tokens;
-    }
-    
-    uint32_t lineIndex = 0;
-    string line;
-    string word="";
-    char c;
-
-    while (std::getline(file,line))
-    {
-        lineIndex++;
-        for (size_t i = 0; i < line.size(); i++)
-        {
-            c = line[i];
-
-            if (isdigit(c) || isalpha(c))
-            {
-                word+=c;
-                continue;
-            }
-
-            if(checkForKeyWord(word))
-            {
-                addToken(counter, word);
-            }
-
-            //zapis do tej pory utworzonego słowa
-            saveToken(tokens,word);
-            //spacja lub koniec lini
-            if (c == ' ' || c == '\n')
-            {
-                continue;
-            }
-            //jest to pojedyńczy operator to zapisz
-            if (single.find(c) != string::npos)
-            {
-                saveToken(tokens,c);
-                continue;
-            }
-            //jest to jeden z operatorów podwójnych
-            if (doubleOperators.find(c) != string::npos)
-            {
-                char n = next(line,i);
-                if (n == '=')
-                {
-                    string s;
-                    s+=c,s+=n;
-                    saveToken(tokens,s);
-                    i++;
-                    continue;
-                }
-
-                if (c == '+' && n == '+')
-                {
-                    string s;
-                    s+=c,s+=n;
-                    saveToken(tokens,s);
-                    i++;
-                    continue;
-                }
-
-                if (c == '-' && n == '-')
-                {
-                    string s;
-                    s+=c,s+=n;
-                    saveToken(tokens,s);
-                    i++;
-                    continue;
-                }
-
-                if (c == '/' && n == '/')
-                {
-                    break;
-                }
-
-                saveToken(tokens,c);
-                continue;
-            }
-             
-            cerr<<src<<':'<<lineIndex<<':'<<i<<" warning: "<<c<<": invalid character\n";         
-        }
-    }
-
-    file.close();
-
-    printCounter(counter);
-
-    return tokens;
-}
 //zwraca true jeśli podane słowo jest słowem kluczowym
 bool checkForKeyWord(const string& word)
 {
@@ -285,8 +134,140 @@ bool checkForKeyWord(const string& word)
     
     return false;
 }
+//wypisuje występowania poszczegulnych keywordow
+void printCounter(const std::map<string, int>& counter)
+{
+    for (auto& keyword : counter)
+    {
+        cout<<keyword.first <<": " <<keyword.second<<"\n";
+    }
+}
+//zapisuje słowo do listy wyrażeń
+void saveToken(std::vector<string>& t,string& word)
+{
+    if (word.size() == 0)
+    {
+        return;
+    }
 
+    t.push_back(word);
 
+    word.clear();
+}
+//zapisuje znak do listy wyrażeń
+void saveToken(std::vector<string>& t,char c)
+{
+    t.push_back(string(1,c));
+}
+//odczytuje następny znak z słowa
+char next(const string& line, const int pos)
+{
+    if (pos >= line.size())
+    {
+        return -1;
+    }
+
+    return line[pos+1];
+}
+//zwraca liste zawierającą poszczegolne "wyrażenia"
+std::vector<string> createTokensFromFile(const string& src)
+{
+    std::vector<string> tokens;
+    std::map<string,int> counter;
+
+    std::ifstream file(src);
+    if (!file.is_open())
+    {
+        cerr<<"error: "<<src<<": No such file or directory\n";
+        return tokens;
+    }
+    
+    uint32_t lineIndex = 0;
+    string line;
+    string word="";
+    char c;
+
+    while (std::getline(file,line))
+    {
+        lineIndex++;
+        for (size_t i = 0; i < line.size(); i++)
+        {
+            c = line[i];
+
+            if (isdigit(c) || isalpha(c))
+            {
+                word+=c;
+                continue;
+            }
+            //zliczanie slow kluczowych
+            if(checkForKeyWord(word))
+            {
+                ++counter[word];
+            }
+
+            //zapis do tej pory utworzonego słowa
+            saveToken(tokens,word);
+            //spacja lub koniec lini
+            if (c == ' ' || c == '\n')
+            {
+                continue;
+            }
+            //jest to pojedyńczy operator to zapisz
+            if (single.find(c) != string::npos)
+            {
+                saveToken(tokens,c);
+                continue;
+            }
+            //jest to jeden z operatorów podwójnych
+            if (doubleOperators.find(c) != string::npos)
+            {
+                char n = next(line,i);
+                if (n == '=')
+                {
+                    string s;
+                    s+=c,s+=n;
+                    saveToken(tokens,s);
+                    i++;
+                    continue;
+                }
+
+                if (c == '+' && n == '+')
+                {
+                    string s;
+                    s+=c,s+=n;
+                    saveToken(tokens,s);
+                    i++;
+                    continue;
+                }
+
+                if (c == '-' && n == '-')
+                {
+                    string s;
+                    s+=c,s+=n;
+                    saveToken(tokens,s);
+                    i++;
+                    continue;
+                }
+
+                if (c == '/' && n == '/')
+                {
+                    break;
+                }
+
+                saveToken(tokens,c);
+                continue;
+            }
+             
+            cerr<<src<<':'<<lineIndex<<':'<<i<<" warning: "<<c<<": invalid character\n";         
+        }
+    }
+
+    file.close();
+
+    printCounter(counter);
+
+    return tokens;
+}
 
 int main(int argc, char* argv[])
 {
