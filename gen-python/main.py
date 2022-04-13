fa5a9e8c5fdc597165122e14925ea2d4078aecf6
TymTel <tymjanek2001@gmail.com>
2022-04-13 09:05:23 +0200

    basic tree check

--- gen-python/main.py
+++ gen-python/main.py
@@ -5,20 +5,50 @@ from grammar.ShaperLexer import ShaperLexer
 from grammar.ShaperParser import ShaperParser
 from MyVisitor import MyVisitor
 from WindowMaker import WindowMaker
+from antlr4.error.ErrorListener import ErrorListener
+
+class ShaperErrorListener(ErrorListener):
+    def __init__(self):
+        super(ShaperErrorListener, self).__init__()
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        raise Exception("ERROR: when parsing line %d column %d: %s\n" (line, column, msg))
+        
+    def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
+        raise Exception("Oh no!")
+
+    def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
+        raise Exception("Oh no!!")
+
+    def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
+        raise Exception("Oh no!!!")
  
 def main(argv):
-    
+
     if len(argv)>1 and os.path.isfile(argv[1]):
         input_stream = FileStream(argv[1])
 
+        error_listener =    ShaperErrorListener()
         lexer = ShaperLexer(input_stream)
+        lexer.removeErrorListeners()
+        lexer.addErrorListener(error_listener)
+
         stream = CommonTokenStream(lexer)
+
         parser = ShaperParser(stream)
-        tree = parser.programm()
+        parser.removeErrorListeners()
+        parser.addErrorListener(error_listener)
+        
+        try:
+            tree = parser.programm()
+        except Exception:
+            print("samethink go wrong")
+            return
 
         myVisitor = MyVisitor()
-
+        
         myVisitor.visit(tree)
+            
 
         myVisitor.window.show()
 
